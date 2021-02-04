2706274532d57eba606b369b569077a13bc0abde
uannabi <zahid_sc@hotmail.fr>
2021-02-04 18:09:29 +0600

    boomcast and adareach done

--- test.py
+++ test.py
@@ -1,10 +1,28 @@
 import requests
+from datetime import datetime
 
 
 def call_pylenin(event=None, context=None):
-    r = requests.get(
-        "https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=Sh@g0R21AdmiN&From=adareach&To=8801833182268&Message=FomAWS")
-    return r.status_code
+    tm1=datetime.now().strftime("%H:%M:%S")
+    txt1 = 'ada reach system'
+    url1='https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=Sh@g0R21AdmiN&From=adareach&To=8801833182268&Message='+tm1+' '+txt1+' OK'
+    r1 = requests.get(url1)
+    mobireach= r1.status_code
 
+    tm2=datetime.now().strftime("%H:%M:%S")
+    txt2 = 'boomcast system'
+    url2=('http://45.249.101.2/boomcast/WebFramework/boomCastWebService/externalApiSendSMSMobiReach?masking=EBL.&userName=robimobireach&password=ec9fdbc5aa84840418b1a5c315655835&MsgType=UNICODE&receiver=8801717502174&message='+tm2+' '+txt2+' OK')
+    r2= requests.get(url2)
+    boomcast = r2.status_code
+    # result:{
+    #     'mobireach':mobireach,
+    #     'boomcast':boomcast
+    # }
+
+
+    return mobireach,boomcast
 
 print(call_pylenin())
+
+
+
