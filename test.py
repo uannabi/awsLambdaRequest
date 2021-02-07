f1966f6d5d340294fe89e548ad9f2c60ea1593fd
uannabi <zahid_sc@hotmail.fr>
2021-02-07 16:09:37 +0600

    set bd time

--- test.py
+++ test.py
@@ -1,28 +1,28 @@
 import requests
 from datetime import datetime
 
+tz_info = {'BD': 'Asia/Dhaka'}
+
 
 def call_pylenin(event=None, context=None):
-    tm1=datetime.now().strftime("%H:%M:%S")
+    tm1 = datetime.now().strftime("%H:%M:%S")
     txt1 = 'ada reach system'
-    url1='https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=Sh@g0R21AdmiN&From=adareach&To=8801833182268&Message='+tm1+' '+txt1+' OK'
+    url1 = 'https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=Sh@g0R21AdmiN&From=adareach&To=8801833182268&Message=' + tm1 + ' ' + txt1 + ' OK'
     r1 = requests.get(url1)
-    mobireach= r1.status_code
+    mobireach = r1.status_code
 
-    tm2=datetime.now().strftime("%H:%M:%S")
+    tm2 = datetime.now().strftime("%H:%M:%S")
     txt2 = 'boomcast system'
-    url2=('http://45.249.101.2/boomcast/WebFramework/boomCastWebService/externalApiSendSMSMobiReach?masking=EBL.&userName=robimobireach&password=ec9fdbc5aa84840418b1a5c315655835&MsgType=UNICODE&receiver=8801717502174&message='+tm2+' '+txt2+' OK')
-    r2= requests.get(url2)
+    url2 = (
+                'http://45.249.101.2/boomcast/WebFramework/boomCastWebService/externalApiSendSMSMobiReach?masking=EBL.&userName=robimobireach&password=ec9fdbc5aa84840418b1a5c315655835&MsgType=UNICODE&receiver=8801515694060&message=' + tm2 + ' ' + txt2 + ' OK')
+    r2 = requests.get(url2)
     boomcast = r2.status_code
     # result:{
     #     'mobireach':mobireach,
     #     'boomcast':boomcast
     # }
 
+    return mobireach, boomcast
 
-    return mobireach,boomcast
 
 print(call_pylenin())
-
-
-
