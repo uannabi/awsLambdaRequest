f5663cdb12ea61e1e49a6a4acb544ded682856e3
uannabi <zahid_sc@hotmail.fr>
2022-11-07 17:23:22 +0600

    new number added

--- test.py
+++ test.py
@@ -14,20 +14,26 @@ def call_pylenin(event=None, context=None):
     tm4 = datetime.now(BD).strftime("%H:%M:%S")
     musking = 'Smart.Lab'
     txt1 = 'CES SMS System'
-    url1 = 'https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=M0MeN@@7ADm1N&From=' + musking + '&To=8801553084509&Message=' + tm1 + ' ' + txt1 + ' OK'
-    url2 = 'https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=M0MeN@@7ADm1N&From=' + musking + '&To=8801825295827&Message=' + tm2 + ' ' + txt1 + ' OK'
-    url3 = 'https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=M0MeN@@7ADm1N&From=' + musking + '&To=8801938388871&Message=' + tm3 + ' ' + txt1 + ' OK'
-    url4 = 'https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=M0MeN@@7ADm1N&From=' + musking + '&To=8801717502174&Message=' + tm4 + ' ' + txt1 + ' OK'
+    url1 = 'https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=@M0MeN@@ADm1n@@&From=' + musking + '&To=8801553084509&Message=' + tm1 + ' ' + txt1 + ' OK'
+    url2 = 'https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=@M0MeN@@ADm1n@@&From=' + musking + '&To=8801825295827&Message=' + tm2 + ' ' + txt1 + ' OK'
+    url3 = 'https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=@M0MeN@@ADm1n@@&From=' + musking + '&To=8801938388871&Message=' + tm3 + ' ' + txt1 + ' OK'
+    url4 = 'https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=@M0MeN@@ADm1n@@&From=' + musking + '&To=8801717502174&Message=' + tm4 + ' ' + txt1 + ' OK'
+    url5 = 'https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=@M0MeN@@ADm1n@@&From=' + musking + '&To=8801321317989&Message=' + tm4 + ' ' + txt1 + ' OK'
+    url6 = 'https://api.mobireach.com.bd/SendTextMessage?Username=shagor&Password=@M0MeN@@ADm1n@@&From=' + musking + '&To=8801404441730&Message=' + tm4 + ' ' + txt1 + ' OK'
     r1 = requests.get(url1)
-    mobireach09 = r1.status_code
     r2 = requests.get(url2)
     r3 = requests.get(url3)
     r4 = requests.get(url4)
+    r5 = requests.get(url5)
+    r6 = requests.get(url6)
+    mobireach09 = r1.status_code
     mobireach27 = r2.status_code
     mobireach71 = r3.status_code
     mobireach74 = r4.status_code
+    mobireach89 = r5.status_code
+    mobireach30 = r6.status_code
 
-    return mobireach09, mobireach27, mobireach71, mobireach74
+    return mobireach09, mobireach27, mobireach71, mobireach74, mobireach89, mobireach30
 
 
 print(call_pylenin())
